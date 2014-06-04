c107c0095b45fc9acb5abf119f9e0d93882c78d1
Jan Koehler <koehler@physik.uni-kiel.de>
2014-06-04 17:58:25 +0200

    Added UI elements, not implemnted yet. Added new particle types (so far only dummy interactions for neutrons), introducd a world volume

--- LD50.py
+++ LD50.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import sys, os
+import sys
 from PyQt4 import QtGui, QtCore
-
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 
-
-progname = os.path.basename(sys.argv[0])
-progversion = "0.1"
 
 
 class MyMplCanvas(FigureCanvasQTAgg):
@@ -17,17 +13,12 @@ class MyMplCanvas(FigureCanvasQTAgg):
         from matplotlib.figure import Figure
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_axes([0,0,1,1])
-        #self.axes = fig.add_subplot(111)
         self.axes.set_xlim(0,1)
         self.axes.set_ylim(0,1)        
         self.axes.set_xticks([])
         self.axes.set_yticks([])
-        # We want the axes cleared every time plot() is called
-        #self.axes.hold(False)
-
         self.compute_initial_figure()
 
-        #
         FigureCanvasQTAgg.__init__(self, fig)
         self.setParent(parent)
 
@@ -46,42 +37,41 @@ class ParticlePlotCanvas(MyMplCanvas):
     """
     """
     def __init__(self, *args, **kwargs):
-        from physics import ChargedParticle, Volume, MeV, cm, amu, q_e, deg
-        self.volume = Volume("torso2.png")
-        self.particle = ChargedParticle(1*amu, 1*q_e, 150*MeV, 
-                                        [0, 60*cm], 0*deg, self.volume)
+        from physics import Particle,  MeV, cm, amu, q_e, deg, WORLD
+        self.particle = Particle(1*amu, 1*q_e, 150*MeV, 
+                                        [0, 60*cm], 0*deg)
         self.path = []
         self.dE = [] 
         MyMplCanvas.__init__(self, *args, **kwargs)
-        self.axes.set_xlim(0, self.volume.image.shape[1])
-        self.axes.set_ylim(0, self.volume.image.shape[0])    
+        self.axes.set_xlim(0, WORLD.image.shape[1])
+        self.axes.set_ylim(0, WORLD.image.shape[0])    
         self.timer = QtCore.QTimer(self)
         QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.update_figure)
 
     def compute_initial_figure(self):
         from matplotlib.pyplot import imread
+        from physics import WORLD
         image = imread("torso2.png")
-        self.axes.imshow(image, extent=(0, self.volume.image.shape[1], 0, self.volume.image.shape[0]))
+        self.axes.imshow(image, extent=(0, WORLD.image.shape[1], 0, WORLD.image.shape[0]))
         self.p_line = self.axes.plot([], [], 'bo')[0]
         self.scat = self.axes.scatter([], [], s=[], c='red',
                                       alpha=0.4, linewidth=0)
     def update_figure(self):
-        # Build a list of 4 random integers between 0 and 10 (both inclusive)
-        from physics import MeV, mm
+        from physics import MeV, mm, WORLD
         if self.particle.energy <= 0:
             self.p_line.set_data([], [])
             self.draw()
             self.timer.stop()
             return            
         x, y, dE = self.particle.step(10.*mm) 
-        if x <0 or x/mm >self.volume.image.shape[1] or y<0 or y/mm>self.volume.image.shape[0]:
+        if x <0 or x/mm >WORLD.image.shape[1] or y<0 or y/mm>WORLD.image.shape[0]:
             self.timer.stop()
             return
         if dE/MeV>0.1:
-            self.dE.append((dE/MeV)**2/4)   
+            self.dE.append((dE/MeV)**2/4) #area->radius   
             self.path.append((x/mm, y/mm))
         self.scat.set_offsets(self.path)
-        self.scat._sizes = self.dE #area->radius
+        self.scat._sizes = self.dE 
         self.p_line.set_data([self.particle.pos_x/mm], [self.particle.pos_y/mm])
         self.draw()
 
@@ -119,18 +109,37 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.selector = QtGui.QComboBox()
         self.selector.addItem("Proton")
         self.selector.addItem("Alpha")
-        self.selector.addItem("Kosmische Strahlung")
+        self.selector.addItem("Elektron")
+        self.selector.addItem("Muon")
+        self.selector.addItem("Neutron")
         ui_grid.addWidget(self.selector, 0, 1)
         ui_grid.addWidget(QtGui.QLabel("Energy / MeV"), 1, 0)
-        self.energy = QtGui.QLineEdit("150")
+        self.energy = QtGui.QLineEdit("50")
         ui_grid.addWidget(self.energy)
+        
+        ui_grid.addWidget(QtGui.QLabel("Schaden in:"))
+        self.b_gray = QtGui.QRadioButton("Gray (Dosis)")
+        self.b_gray.setChecked(True)
+        self.b_sievert = QtGui.QRadioButton("Sievert (equivalent Dosis)")
+        ui_grid.addWidget(self.b_gray, 2, 1)
+        ui_grid.addWidget(self.b_sievert, 3, 1)
+        
+        
+        ui_grid.addWidget(QtGui.QLabel("Anzahl Ereignisse:"), 4, 0)    
+        self.b_shots = QtGui.QSpinBox()
+        self.b_shots.setValue(1)
+        self.b_shots.setMinimum(1)
+        self.b_shots.setMaximum(100)
+        self.b_shots.setEnabled(False)
+        ui_grid.addWidget(self.b_shots, 4, 1)        
         
         btn_start = QtGui.QPushButton("Start")
         btn_start.clicked.connect(self.start_run)
         btn_clear = QtGui.QPushButton("Clear")
         btn_clear.clicked.connect(self.clear)
-        ui_grid.addWidget(btn_start, 2, 0)
-        ui_grid.addWidget(btn_clear, 2, 1)
+        ui_grid.addWidget(btn_start, 5, 0)
+        ui_grid.addWidget(btn_clear, 5, 1)
+        
                 
         
         ui_widget = QtGui.QWidget(self.main_widget)
@@ -160,19 +169,20 @@ A tool to visualize the effect of radiation on the human body.
 """
 )
     def start_run(self):
+        #for i in xrange(self.b_shots.value()):
         self.rad_plot.timer.stop()
         self.particle_generator()
         self.rad_plot.timer.start(10)
+
     def clear(self):
         self.rad_plot.timer.stop() 
         self.rad_plot.clear_figure()
     def particle_generator(self):
-        from physics import ChargedParticle, cos_law, mm
+        from physics import Particle, cos_law, mm
         from physics import MeV, amu, q_e, deg
         from numpy.random import rand
         energy = float(self.energy.text())
         pos = (rand()*1200*2+800)*mm
-        pos = rand()*2000*mm
         if pos < 1200*mm:
             direction = cos_law()
             pos_x = 0
@@ -181,23 +191,33 @@ A tool to visualize the effect of radiation on the human body.
             direction = cos_law() + 270*deg
             pos_x = pos-1200*mm
             pos_y = 1200*mm
-        else:
+        else:   
             direction = cos_law() + 180*deg
-            pos_x = 600*mm
+            pos_x = 800*mm
             pos_y = pos -2000*mm
         
-
         if self.selector.currentText()=="Proton":
-            self.rad_plot.particle = ChargedParticle(1*amu, 1*q_e, energy*MeV, 
-                                        [pos_x, pos_y], direction, self.rad_plot.volume)
+            self.rad_plot.particle = Particle(1*amu, 1*q_e, energy*MeV, 
+                                        [pos_x, pos_y], direction)
         elif self.selector.currentText()=="Alpha":
-            self.rad_plot.particle = ChargedParticle(4*amu, 2*q_e, energy*MeV, 
-                                        [pos_y, pos_y], direction, self.rad_plot.volume)            
-
+            self.rad_plot.particle = Particle(4*amu, 2*q_e, energy*MeV, 
+                                        [pos_x, pos_y], direction)
+        elif self.selector.currentText()=="Elektron":
+            from physics import m_e
+            self.rad_plot.particle = Particle(m_e, q_e, energy*MeV, 
+                                        [pos_x, pos_y], direction)
+        elif self.selector.currentText()=="Muon":
+            from physics import m_muon
+            self.rad_plot.particle = Particle(m_muon, q_e, energy*MeV, 
+                                        [pos_x, pos_y], direction)
+        elif self.selector.currentText()=="Neutron":
+            from physics import Neutron
+            self.rad_plot.particle = Neutron(amu, 0, energy*MeV, 
+                                        [pos_x, pos_y], direction)
+                                        
 qApp = QtGui.QApplication(sys.argv)
 
 aw = ApplicationWindow()
-#aw.setWindowTitle("%s" % progname)
 aw.show()
 sys.exit(qApp.exec_())
 #qApp.exec_()
--- physics.py
+++ physics.py
@@ -4,7 +4,9 @@ Created on Sun Jun 01 11:24:13 2014
 
 @author: koehler
 """
-from scipy.constants import atomic_mass as amu, e as q_e, pi
+from scipy.constants import atomic_mass as amu, e as q_e, pi, m_e
+from scipy.constants import physical_constants
+m_muon = physical_constants['muon mass'][0]
 
 mm = 1e-3
 cm = 1e-2
@@ -17,6 +19,7 @@ m3 = 1
 g =1e-3
 kg = 1
 
+eV = q_e
 MeV = q_e*1e6
 
 deg = 2*pi/360
@@ -40,10 +43,10 @@ class Material(object):
         self.A = A
         self.rho = rho
     def get_I(self):
-        """bethe"""
+        """important for bethe"""
         return 10 * q_e * self.Z
     def get_n(self):
-        """ bethe"""
+        """important for bethe"""
         return self.Z*self.rho/self.A/amu
         
 class Volume(object):
@@ -63,17 +66,27 @@ class Volume(object):
             return True
         else:
             return False
+    def get_In(self, pos_x, pos_y):
+        if self.is_inside(pos_x, pos_y):
+            return self.material.get_I(), self.material.get_n()
+        else:
+            return 1e-30, 0 
+    def get_neutron_mfp(self, pos_x, pos_y):
+        if self.is_inside(pos_x, pos_y):
+            return 10*cm
+        else:
+            return 1e30
 
+WORLD = Volume('torso2.png', material=Material(8., 16., 1.*g/cm3))
 
-class ChargedParticle(object):
-    def __init__(self, mass, charge, energy, pos, direction, volume):
+class Particle(object):
+    def __init__(self, mass, charge, energy, pos, direction):
         self.mass = mass
         self.charge = charge    #e
         self.energy = energy
         self.pos_x = pos[0]
         self.pos_y = pos[1]
         self.dir = direction
-        self.volume = volume
         
         self.path = []
         self.dE = []
@@ -83,35 +96,57 @@ class ChargedParticle(object):
         Make a ds step, return new pos and energy loss
         """
         import numpy as np
-        if self.volume.is_inside(self.pos_x, self.pos_y):
-            dE = self.bethe()* ds
-            if dE > self.energy:
-                dE = self.energy
-            self.energy -= dE
-        else: 
-            dE = 0
+
+        dE = self.energy_loss()* ds
+        if dE > self.energy:
+            dE = self.energy
+        self.energy -= dE
+
         pos_x = self.pos_x
         pos_y = self.pos_y
         self.pos_x += np.cos(self.dir)*ds
         self.pos_y += np.sin(self.dir)*ds
-        self.path.append((pos_x, pos_y))
-        self.dE.append(dE)
         return pos_x, pos_y, dE
     
     def get_velocity(self):
         from scipy.constants import c
         from numpy import sqrt
         return sqrt(1-(self.energy/(self.mass*c**2)+1)**-2)*c
-    def bethe(self):
+    def energy_loss(self):
         """
+        Beethe Bloch
         """
         from numpy import log
-        from scipy.constants import epsilon_0, c, pi, m_e
-        I = self.volume.material.get_I()
-        n = self.volume.material.get_n()
+        from scipy.constants import epsilon_0, c, pi
+        I, n = WORLD.get_In(self.pos_x, self.pos_y)
         v = self.get_velocity()
         z = self.charge/q_e
         beta = v/c
         return 4*pi*n*z**2/(m_e*c**2*beta**2)*(q_e**2/(4*pi*epsilon_0))**2* \
             (log(2*m_e*c**2*beta**2/I/(1-beta**2))-beta**2)
             
+class Neutron(Particle):
+    def step(self, ds):
+        import numpy as np
+        from numpy.random import rand
+        dE = self.energy_loss(ds)
+        if dE > self.energy-10*eV:
+            dE = self.energy
+        self.energy -= dE
+        if dE:
+            self.dir += rand()*80*deg-40*deg
+        pos_x = self.pos_x
+        pos_y = self.pos_y
+        self.pos_x += np.cos(self.dir)*ds
+        self.pos_y += np.sin(self.dir)*ds
+        return pos_x, pos_y, dE        
+    def energy_loss(self, ds):
+        """
+        straggeling
+        """
+        from numpy.random import rand
+        dE = 0
+        mfp = WORLD.get_neutron_mfp(self.pos_x, self.pos_y)
+        if ds/mfp> rand():
+            dE = min((20*MeV, self.energy*rand()))
+        return dE
